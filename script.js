fc0d639cb0a6c47bb078b27fadc47443c7a70c25
Ruso Chanturidze <108417178+r140592uso@users.noreply.github.com>
2022-11-01 22:06:29 +0400

    1

--- script.js
+++ script.js
@@ -10,15 +10,27 @@ togglebar.addEventListener("click", function () {
 
 // scroll function
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 let navbarscroll = document.getElementById("nav-ul");
 let sticky = navbarscroll.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbarscroll.classList.add("sticky")
+    navbarscroll.classList.add("sticky");
   } else {
     navbarscroll.classList.remove("sticky");
   }
 }
+
+// articles
+fetch("./articles.json")
+  .then((res) => {
+    return res.json();
+  })
+  .then((Data) => console.log(Data))
+  .catch((error) => console.log("ERROR"));
+
+// "https://newsapi.org/v2/everything?q=apple&from=2022-10-30&to=2022-10-30&sortBy=popularity&apiKey=c0f2c7d2abf7466584810b33703f8bbd"
