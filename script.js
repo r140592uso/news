c1dd0eacf8e7d545c4c71de2c5f66bd9ff42b04f
Ruso Chanturidze <108417178+r140592uso@users.noreply.github.com>
2022-11-04 00:08:43 +0400

    1

--- script.js
+++ script.js
@@ -26,11 +26,42 @@ function myFunction() {
 }
 
 // articles
-fetch("articles.json")
-  .then((res) => {
-    return res.json();
+fetch("/Json/articles1.json", {
+  method: "GET",
+})
+  .then(function (response) {
+    if (response.status !== 200) {
+      throw response.status;
+    }
+    return response.json();
   })
-  .then((Data) => console.log(Data))
-  .catch((error) => console.log("ERROR"));
+  .then(function (responseData) {
+    console.log(responseData);
+    responseData.articles.forEach((element) => {
+      let ul = document.getElementById("news");
+
+      let li = document.createElement("li");
+      li.classList.add("newsli");
+
+      let h3 = document.createElement("h3");
+      h3.textContent = `${element.title}`;
+      h3.classList.add("newstitle");
+
+      let image = document.createElement("img");
+      image.classList.add("newsimage");
+      image.setAttribute("src", element.urlToImage);
+      image.setAttribute("alt", element.title);
+
+      li.appendChild(image);
+      li.appendChild(h3);
+      ul.appendChild(li);
+    });
+  })
+  .catch((error) => {
+    let p = document.createElement("p");
+    p.textContent = "Server Error";
+    p.style.color = "red";
+    document.getElementById("api").appendChild(p);
+  });
 
 // "https://newsapi.org/v2/everything?q=apple&from=2022-10-30&to=2022-10-30&sortBy=popularity&apiKey=c0f2c7d2abf7466584810b33703f8bbd"
